1f3e547d0ae5843af1408f861902874be437d7bb
Marion Livet <mlivet93@gmail.com>
2013-10-16 23:29:49 +0200

    Edit fichiers map
    Ajout de fonctionnalités (limites de la map et ajout du commentaire pour le checkin)

--- map/geolocation.js
+++ map/geolocation.js
@@ -1,12 +1,13 @@
 var map;
 var initializeGeolocation;
 var latLng;
+var minZoomLvl = 12;
 
 // initialisation de la carte
 initializeGeolocation = function(){
 	latLng = new google.maps.LatLng(48.857261,2.341751); 
 	var myOptions = {
-	    zoom      : 13, 
+	    zoom      : minZoomLvl, 
 	    center    : latLng, 
 	    mapTypeId : google.maps.MapTypeId.ROADMAP, 
 	    styles: styles,
@@ -16,18 +17,53 @@ initializeGeolocation = function(){
   
     map      = new google.maps.Map(document.getElementById('map-canvas'), myOptions);
 
-//MARQUEURS SUR LA CARTE
-	var rep = new google.maps.Marker({
-	    position : new google.maps.LatLng(48.869006,2.364067),
-	    map      : map,
-	    title    : "République",
-	});
 
-	var bastille = new google.maps.Marker({
-	    position : new google.maps.LatLng(48.85311,2.36965),
-	    map      : map,
-	    title    : "Bastille",
-	});
+  // Frontières de la carte --- http://stackoverflow.com/questions/3818016/google-maps-v3-limit-viewable-area-and-zoom-level
+   var strictBounds = new google.maps.LatLngBounds(
+     new google.maps.LatLng(48.816811,2.251114), 
+     new google.maps.LatLng(48.902643,2.408699)
+   );
+
+  // Listen for the dragend event
+   google.maps.event.addListener(map, 'dragend', function() {
+     if (strictBounds.contains(map.getCenter())) return;
+
+     // We're out of bounds - Move the map back within the bounds
+
+     var c = map.getCenter(), //récupère coord. du centre de la carte
+         x = c.lng(), //récupère lng actuelle du centre
+         y = c.lat(), //idem avec lat
+         maxX = strictBounds.getNorthEast().lng(), //lng de l'angle en haut à droite de la map
+         maxY = strictBounds.getNorthEast().lat(), //lat''''''''
+         minX = strictBounds.getSouthWest().lng(), //lng de l'angle en bas à gauche de la map
+         minY = strictBounds.getSouthWest().lat(); //lat''''''''
+
+      //si le centre de la map est hors des frontières
+     if (x < minX) x = minX;
+     if (x > maxX) x = maxX;
+     if (y < minY) y = minY;
+     if (y > maxY) y = maxY;
+
+     map.setCenter(new google.maps.LatLng(y, x));
+   });
+
+// Empêcher de trop dézoomer
+   google.maps.event.addListener(map, 'zoom_changed', function() {
+     if (map.getZoom() < minZoomLvl) map.setZoom(minZoomLvl);
+   });
+
+//MARQUEURS SUR LA CARTE
+  var rep = new google.maps.Marker({
+      position : new google.maps.LatLng(48.869006,2.364067),
+      map      : map,
+      title    : "République",
+  });
+
+  var bastille = new google.maps.Marker({
+      position : new google.maps.LatLng(48.85311,2.36965),
+      map      : map,
+      title    : "Bastille",
+  });
 
 //INFOBULLE REPUBLIQUE
 //Contenu
@@ -71,29 +107,51 @@ function handleError(error) {
 		break;
 	}
 }
-        
+ var latitude, longitude;       
 //Récupération des données de localisation
 function findPosition(position) {
 	latitude = position.coords.latitude;
 	longitude = position.coords.longitude;
 	accuracy = position.coords.accuracy;
   findOnGoogleMaps();
-var xhr = new XMLHttpRequest();
-var url = "ajout_checkin.php?lat=" + latitude + "&lng=" + longitude;
-xhr.open("GET", url);
-xhr.send(null);
+//var xhr = new XMLHttpRequest();
+//var url = "trouve_position.php?lat=" + latitude + "&lng=" + longitude;
+//xhr.open("GET", url);
+//xhr.send(null);
 }
 	
 	//Message check in
 var checkinSuccess = '<div id="content">'+
-      '<div id="siteNotice">'+
-      '</div>'+
-      '<h1 id="firstHeading" class="firstHeading">Tu es ici</h1>'+
-      '</div>';
+      '<h1 id="firstHeading" class="firstHeading">Tu es ici. Wanna checkin?</h1>'+
+      '<div id="textInfoCheckin">'+
+      '<form name="add_comment" ><label for="checkin_comment">Commentaire (<140 car.) :</label> <input type="text" id="checkin_comment" name="checkin_comment" /><br>'+
+      '<a href="#" onclick="addCheckin()">OK</a></form></div>'+
+      '</div>';///////////////SI CONFIRME, ALORS requete AJAX sur page PHP pour mettre les infos & comm dans BDD
+
 //Création de l'infobulle
   var infowindowCI = new google.maps.InfoWindow({
       content: checkinSuccess
   });
+
+function addCheckin(){
+  //cancelDefaultAction(); //annule l'action du clic
+  var comment = document.forms["add_comment"].elements[0].value;
+  var url = "add_checkin.php?lat=" + latitude + "&lng=" + longitude+ "&comm=" + comment;
+  var xhr = new XMLHttpRequest();
+  xhr.open("GET", url);
+  xhr.send(null);
+  document.getElementById('firstHeading').innerHTML="C'est fait !";
+  document.getElementById('textInfoCheckin').innerHTML="Check in enregistré !";
+}
+
+
+//annule l'action par défaut de l'event js
+function cancelDefaultAction() {
+ //var evt = e ? e:window.event;
+ if (evt.preventDefault) evt.preventDefault();
+ evt.returnValue = false;
+ return false;
+}
 
 //Cherche et affiche la position de l'utilisateur sur la carte
 function findOnGoogleMaps() {
