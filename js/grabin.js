cee458de6234eac441859e53a7d4e4b037e26f01
Romain Barbier <romain.barbier@mail.com>
2013-11-15 16:24:30 +0100

    Modifs inté

---
+++ js/grabin.js
@@ -0,0 +1,18 @@
+/* getDropDownDown(){
+  document.getElementById('connexion-dropdown').className='dropdown-down';
+}
+
+function getDropDownUp(){
+  document.getElementById('connexion-dropdown').className='dropdown-up';
+} */
+
+
+
+function getDropDownDown(id){
+  document.getElementById(id).className='dropdown-down';
+}
+
+
+function getDropDownUp(id){
+  document.getElementById(id).className='dropdown-up';
+}
