a5c8578525579fd0dbdaa6ee3295314a88ac0b45
Marion Livet <mlivet93@gmail.com>
2013-11-18 23:13:54 +0100

    Style recherche

--- chercher.php
+++ chercher.php
@@ -9,29 +9,27 @@ if (!isset($_SESSION['login'])) {
 
 else if(isset($_GET['motclef'])){
 		$recherche=mysql_escape_string($_GET['motclef']);
-		
-		if(isset($_SESSION['profilVisite']))
-			$profilVisite=$_SESSION['profilVisite'];
 			
 		$login=mysql_escape_string($_SESSION['login']);
 		
 		$link=mysql_connect("mysql51-100.perso","robinpayadmin","gUFjHp3Q8m9y");
 		mysql_select_db("robinpayadmin") or die (mysql_error());
 		
-	if(isset($_SESSION['profilVisite'])){
-		$res1=mysql_query("SELECT * FROM grabin_user WHERE pseudo LIKE '$recherche%' AND pseudo <> '$login' AND pseudo <> '$profilVisite'")or die (mysql_error());}
-	else{
-		$res1=mysql_query("SELECT * FROM grabin_user WHERE pseudo LIKE '$recherche%' AND pseudo <> '$login'")or die (mysql_error());
-		}
+		$res1=mysql_query("SELECT * FROM grabin_user WHERE pseudo LIKE '%$recherche%' AND pseudo <> '$login'")or die (mysql_error());
+		
 		
 
 		 if( mysql_num_rows($res1)>=1){
+		 	echo '<ul id="list-results">';
 				while ($util=mysql_fetch_assoc($res1)){
 					$ok=$util['pseudo'];
-					$_SESSION['IDprofilVisite']=$util['id'];
-					echo "<a href="."voirProfil.php?profil=".$ok.">".$ok."</a></br>";
+					$idProfil=$util['id'];
+					$photo=$util['avatar'];
+					
+					echo "<li><a href="."voirProfil.php?profil=".$ok."&key=".$idProfil."><img src=".$photo." width=40px height=40px alt='photo'>".$ok."</a></li>";
 				
 			}
+			echo "</ul>";
 		}
 		 
 		 else {
@@ -41,12 +39,3 @@ else if(isset($_GET['motclef'])){
 }
 			
 ?>
-<html>
-<head>
-<meta charset="utf-8">
-<title>Document sans titre</title>
-</head>
-
-<body>
-</body>
-</html>
