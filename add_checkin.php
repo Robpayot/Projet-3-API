1a8fdcf9a2310d83c27a1c71b6ef1af5140b1cc3
Marion Livet <mlivet93@gmail.com>
2013-11-18 15:12:57 +0100

    Affichage pseudo dans infobulle des checkins

--- add_checkin.php
+++ add_checkin.php
@@ -1,7 +1,9 @@
 <?php 
+session_start();
 require 'config.php';
 
-
+$id_user=$_SESSION['ID'];
+$pseudo=$_SESSION['login'];
 date_default_timezone_set('CET');
 
 function unix_timestamp($date)
@@ -22,8 +24,31 @@ $start = date("Y-m-d H:i");
 $startTS = unix_timestamp($start);
 $endTS = $startTS+10800;
 $end = date("Y-m-d H:i", $endTS);
+$score;
 //echo $lat.", ".$lng."<br>";
 //echo "start: ".$start.", end: ".$end."<br>";
-$dbh->exec("INSERT INTO checkIn(json, lat, lng, comment, date_begin, date_end) VALUES('json', '$lat', '$lng', '$comment', '$start', '$end')"); 
+$dbh->exec("INSERT INTO checkIn(json,id_user, pseudo, lat, lng, comment, date_begin, date_end) VALUES('json','$id_user', '$pseudo' '$lat', '$lng', '$comment', '$start', '$end')"); 
+
+
+		$link=mysql_connect("mysql51-100.perso","robinpayadmin","gUFjHp3Q8m9y");
+		mysql_select_db("robinpayadmin") or die (mysql_error());
+		$res1=mysql_query("SELECT * FROM grabin_user WHERE id='$id_user'")or die (mysql_error());
+	
+		 if( mysql_num_rows($res1)>=1){
+				while ($util=mysql_fetch_assoc($res1)){
+					$score=$util['score']+10;
+				}
+				
+		 }
+		 
+		 $res1=mysql_query("UPDATE grabin_user SET score='$score' WHERE id='$id_user'")or die (mysql_error());
+/*$dbh -> exec("SELECT * FROM grabin_user WHERE id='$id_user'")->fetchAll();
+
+foreach ($user as $users):
+$score=$users['score']+10;
+
+endforeach;
+
+$dbh -> exec("UPDATE grabin_user SET score='$score' WHERE id='$id_user'");*/
        
 ?>
--- get_checkin.php
+++ get_checkin.php
@@ -2,7 +2,7 @@
 require 'config.php';
 
 // lancement de la requete
-$sth = $dbh->prepare("SELECT json, id, id_user, lat, lng, comment, date_begin, date_end FROM checkIn");
+$sth = $dbh->prepare("SELECT json, id, id_user, pseudo, lat, lng, comment, date_begin, date_end FROM checkIn");
 $sth->execute();
 $data=$sth->fetchAll(PDO::FETCH_ASSOC|PDO::FETCH_GROUP);
 echo json_encode($data);
