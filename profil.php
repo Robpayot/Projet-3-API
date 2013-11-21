1cf5edc8ed56e3803a9dc67bc38814ea249e025c
Marion Livet <mlivet93@gmail.com>
2013-11-21 14:58:01 +0100

    Intégration profil.php
    ajout des formulaires d'upload, de changement de statut, la météo et liste des checkins à venir

--- profil.php
+++ profil.php
@@ -42,36 +42,51 @@ require 'topbar.php' ;
 
                         
                 </div>
-                <div><p id="status">&#171;<?php echo $statut ?>!&#187;</p></div> 
-
+                <div id="statut">
+                  <p id="status">&#171;Bon, il faut encore que je travaille mes réceptions, mais je rentre le Backflip !&#187;</p> 
+                  <p><input type="text" id="newStatut" name="newStatut" placeholder="Nouveau statut" required ><br>
+                  <input type="button" id="envoiStatut" name="envoiStatut" value="Exprime-toi !" class="button"/></p>
+                </div>
                 <div id="cal">
                         <div id="cal-shadow-1"></div>
                         <div id="cal-shadow-2"></div>
-                        <div><p>Température:<?php echo $temperature ?></p>
-                        	<p>Code Image:<?php echo $codeImg ?></p>
+                        <div id="weather">
+                            <span>Météo</span>
+                            <span>Paris</span>
+                            <span><?php echo $temperature ?></span>
+                            <img src="http://l.yimg.com/a/i/us/we/52/<?php echo $codeImg ?>.gif"/>
                         </div>
                 </div>
         </div>
 </div>
 <div id="profile-content">
 
+  <h3>Tes prochains checkins</h3>
 
-<input type="text" id="newStatut" name="newStatut" placeholder="Exprime toi !" required >
-        <input type="button" id="envoiStatut" name="envoiStatut" value="POSTER" class="button"/>
-                
-                <form action="profil.php" enctype="multipart/form-data" method="post">
-                <input type="hidden" name="MAX_FILE_SIZE" value="100000000">
-     <p>Poster une photo : <input type="file" name="url" id="url" required></p>
-        <input name="description" id="description" data-provide="limit" data-counter="#counter"  rows="8" required></input>
-             <button id="submit_photo" name="submit_photo" type="submit">Valider</button></p>
-                </form> 
-                <form action="profil.php" enctype="multipart/form-data" method="post">
-                <input type="hidden" name="MAX_FILE_SIZE" value="100000000">
-    <p> Ou une vidéo (fichiers mp4 et ogg) : <input type="file" name="url" id="url" required></p>
-        <input name="description" id="description" data-provide="limit" data-counter="#counter"  rows="8" required></input>
-             <button id="submit_video" name="submit_video" type="submit">Valider</button></p>
-                </form> 
+  <div id="list-checkins"></div>
 
+<h3 class="inline-block">Photos et vidéos</h3>
+
+<a href="#" class="poster-media" onclick="return afficher_cacher('poster-photo');">Poster une photo</a>
+<a href="#" class="poster-media" onclick="return afficher_cacher('poster-video'); ">Poster une vidéo</a>
+
+<div id="poster-photo" class="upload-media" style="display:none">
+  <form action="profil.php" enctype="multipart/form-data" method="post">
+    <input type="hidden" name="MAX_FILE_SIZE" value="100000000">
+    <p><label for="url">Votre photo : </label><input type="file" name="url" id="url" required></p>
+    <p><label for="description">Description : </label><input name="description" id="description" data-provide="limit" data-counter="#counter"  rows="8" ></input></p>
+    <button id="submit_photo" name="submit_photo" type="submit">Valider</button></p>
+  </form> 
+</div>
+
+<div id="poster-video" class="upload-media" style="display:none">
+  <form action="profil.php" enctype="multipart/form-data" method="post">
+    <input type="hidden" name="MAX_FILE_SIZE" value="100000000">
+    <p><label for="url">Votre vidéo (fichiers mp4 et ogg) :</label> <input type="file" name="url" id="url" required></p>
+    <p><label for="description">Description : </label><input name="description" id="description" data-provide="limit" data-counter="#counter"  rows="8" ></input></p>
+    <button id="submit_video" name="submit_video" type="submit">Valider</button></p>
+  </form> 
+</div>
 
   <div id="medias">
     <!--// Gallery Markup: A container that the plugin is called upon, and two lists for the images (use images with same aspect ratio) //-->
@@ -106,10 +121,6 @@ require 'topbar.php' ;
       </div>
     </div><!-- end #gallery-container-->    
     <div id="video-section">
-      <?php
-        if (empty($medias['url_vid'])){
-        }else{
-       ?>
       <div id="button" class="pause">
         <span></span>
       </div>
@@ -129,12 +140,11 @@ require 'topbar.php' ;
       <button type="button" id="mute">Mute</button>
       <input type="range" id="volume-bar" min="0" max="1" step="0.1" value="1">
       <button type="button" id="full-screen">Full-Screen</button>
-      <?php } ?>
     </div> <!-- end #video-section-->
 
 
   </div> <!-- end #medias-->
-<img src="http://l.yimg.com/a/i/us/we/52/<?php echo $codeImg ?>.gif"/>
+
 
 
 
