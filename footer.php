01393c0d75bd96680762d7f480cd0e33b4507513
Marion Livet <mlivet93@gmail.com>
2013-11-21 09:47:11 +0100

    Déplacement appel jquery

--- footer.php
+++ footer.php
@@ -1,7 +1,6 @@
 
 
 <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCL6jbConOc2cMBNepwDNA0l_lqrNOaRPI&sensor=true"></script><!-- API Google Maps-->
-<script src="http://code.jquery.com/jquery-1.10.1.min.js"></script>
 <script src="http://code.jquery.com/ui/1.10.3/jquery-ui.js"></script>
 <script type="text/javascript" src="js/verifConnexion.js"></script>
 <script type="text/javascript" src="js/planning.js"></script>
--- header.php
+++ header.php
@@ -6,6 +6,7 @@
 <link rel="stylesheet" href="css/video.css" type="text/css" />
 <link rel="stylesheet" href="css/gallery.css" type="text/css" />
 <link rel="stylesheet" href="http://code.jquery.com/ui/1.10.3/themes/smoothness/jquery-ui.css">
+<script src="http://code.jquery.com/jquery-1.10.1.min.js"></script>
 <script src="js/grabin.js" type="text/javascript"></script>
 <script type="text/javascript" src="http://google-maps-utility-library-v3.googlecode.com/svn/trunk/infobubble/src/infobubble.js"></script>
 
