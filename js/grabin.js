2ec34e33df72b99aab0aae46c11212f803c697ae
Marion Livet <mlivet93@gmail.com>
2013-11-23 23:18:39 +0100

    Suppression lignes répétées

--- js/grabin.js
+++ js/grabin.js
@@ -21,7 +21,6 @@ $('#recherche').focus(displayResults());
 $('#recherche').blur(hideResults());
 $('#resultat').css("opacity","0");
 
-//$( "#date" ).datepicker({ dateFormat: 'dd/mm/yy' });
 
 function getOff(id){
   document.getElementById(id).className='opacity-0';
@@ -32,50 +31,35 @@ function getOn(id){
   document.getElementById(id).className='noclass opacity-0';
   setTimeout(function(){document.getElementById(id).className='opacity-1';},100);
 };
+function getAvatar(){
+  document.getElementById('avatar').className='envoie opacity-0';
+  document.getElementById('close-avatar').className='opacity-0';
+  setTimeout(function(){document.getElementById('avatar').className='envoie opacity-1';},100);
+  setTimeout(function(){document.getElementById('close-avatar').className='opacity-1';},100);
+};
+function getAvatarOff(){
+  document.getElementById('avatar').className='envoie opacity-0';
+  document.getElementById('close-avatar').className='opacity-0';
+  setTimeout(function(){document.getElementById('avatar').className='dspln';},100);
+  setTimeout(function(){document.getElementById('close-avatar').className='dspln';},100);
+};
 
-$('#recherche').focus(displayResults());
-$('#recherche').blur(hideResults());
-$('#resultat').css("opacity","0");
 
-$( "#date" ).datepicker({ dateFormat: 'dd/mm/yy' });
+//$( "#date" ).datepicker({ dateFormat: 'dd/mm/yy' });
 
 
 function afficher_cacher(id){
 	var id1="poster-video";
 	var id2="poster-photo";
-
-//document.getElementById(id1).style.display="none";
-//document.getElementById(id2).style.display="none";
 	
 	if(id==id1)
 		idb=id2;
 	else
 		idb=id1;
 	
-
-//document.getElementById(idb).style.display="none";
-	$( "#"+idb ).stop().fadeOut(100,'easeInSine');
-	
-	$( "#"+id ).stop().delay(100).slideToggle(700,'easeInSine');
-//$( "#"+idb ).stop().slideToggle(1000,'easeInOutBack');
-    	//document.getElementById(id).style.display="none";
-
-
-    /*if(id=='poster-photo'){
-    	if(document.getElementById('poster-video').style.display=="block")
-	    {
-			$( "#poster-video" ).slideToggle(1000,'easeInOutBack');
-	    	//document.getElementById('poster-video').style.display="none";
-	    } 
-    } else if(id=='poster-video'){
-    	if(document.getElementById('poster-photo').style.display=="block")
-	    {
-			$( "#poster-photo" ).slideToggle(1000,'easeInOutBack');
-	    	//document.getElementById('poster-photo').style.display="none";
-	    }
-	} */
-    
-    return false;
+  document.getElementById(idb).style.display="none";
+	$( "#"+id ).stop().slideToggle(500,'easeInSine');
+  return false;
 }
 
 function displayResults(){
